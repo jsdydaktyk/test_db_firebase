4168e36ae93d608d7be1ae74e98ad4ab72b0d383
js <js@cos.pl>
2023-03-03 14:43:07 +0100

    Dalej

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    const [myData, setMyData] = useState('')
+
+  function inputHandler(event){
+    setMyData(event.target.value);
+    
+  }
+
+  async function sendDataHandler(){
+      // event.preventDefault();
+      console.log(myData);
+      const res = await fetch('https://test-7a2ea-default-rtdb.firebaseio.com/pierwsze_kroki.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(myData),
+        headers:{
+          'Content-Type': 'application.json'
+        }
+
+      }) ;
+       const data = await res.json() ;
+       console.log(data) ;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form onSubmit={sendDataHandler}>
+        <input type="text"
+               onChange={inputHandler}
+        />
+        <button type="submit"> Prześlij dane do bazy </button>
+      </form>
     </div>
   );
 }
